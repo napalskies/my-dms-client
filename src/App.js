17dfb2810c1cd940a011cc2b59486c94439a7aba
adinauifi <36963895+adinauifi@users.noreply.github.com>
2025-10-28 16:36:49 +0200

    try again to make gh pages work

--- src/App.js
+++ src/App.js
@@ -1,38 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/pages/Login';
 import Dashboard from './components/pages/Dashboard';
 import FolderCreation from './components/pages/FolderCreation';
 import Upload from './components/pages/Upload';
 import Display from './components/pages/Display';
-import React from 'react';
 import {HashRouter as Router, Routes, Route} from 'react-router-dom';
 import DocumentList from './components/pages/DocumentList'
 
 
 function App() {
   return (
-    <div>
-      {/* <--<p>{props.title}</p>!--> */}
-      {/* <Header name="Adina"/>
-      <Header name="AdinaUifi"/>
-      <nav className="nav">
-        <Link to ="/" className="nav-item">Login</Link>
-        <Link to ="/document-list" className="nav-item">Doc List</Link>
-      </nav> */}
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login/>}/>
-          <Route path="/dashboard" element = {<Dashboard />}>
-            <Route index element={<FolderCreation/>}/>
-            <Route path="create-folder" element={<FolderCreation/>}/>
-            <Route path="upload" element={<Upload/>}/>
-            <Route path="display" element={<Display/>}/>
-          </Route>
-          <Route path="/document-list" element = {<DocumentList/>}/>
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/dashboard" element = {<Dashboard />}>
+          <Route index element={<FolderCreation/>}/>
+          <Route path="create-folder" element={<FolderCreation/>}/>
+          <Route path="upload" element={<Upload/>}/>
+          <Route path="display" element={<Display/>}/>
+        </Route>
+        <Route path="/document-list" element = {<DocumentList/>}/>
+      </Routes>
+    </Router>
   );
 }
 
